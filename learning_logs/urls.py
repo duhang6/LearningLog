ee2ae21a20645d366398d31d69168bbed1756fe7
duhang <hustduhang@gmail.com>
2020-12-21 23:48:54 -0800

    Styling some pages.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -6,8 +6,8 @@ from . import views
 
 app_name = 'learning_logs'
 urlpatterns = [
-	#Home page
-	path('', views.index, name='index'),
+    #Home page
+    path('', views.index, name='index'),
     path('topics/', views.topics, name='topics'),
     path('topics/<int:topic_id>/', views.topic, name='topic'),
     path('new_topic/', views.new_topic, name='new_topic'),
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -10,7 +10,12 @@ from .forms import TopicForm, EntryForm
 # Create your views here.
 def index(request):
 	"""The home page for Learning Log."""
-	return render(request, 'learning_logs/index.html')
+	if request.user.is_authenticated:
+		topics = Topic.objects.filter(owner=request.user).order_by('date_added')
+		context = {'topics': topics}
+		return render(request, 'learning_logs/topics.html', context)
+	else:
+		return render(request, 'learning_logs/index.html')
 
 @login_required
 def topics(request):
